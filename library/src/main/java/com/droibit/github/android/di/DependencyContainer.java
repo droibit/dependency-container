30b0a5f31c070abf19e8c1ecbd96e91f5cf19fea
Shinya Kumgai <roomful.rooms@gmail.com>
2017-02-16 22:39:27 +0900

    Added DependencyContainer’s key

--- library/src/main/java/com/droibit/github/android/di/DependencyContainer.java
+++ library/src/main/java/com/droibit/github/android/di/DependencyContainer.java
@@ -1,5 +1,35 @@
 package com.droibit.github.android.di;
 
 
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+
+import java.lang.reflect.Type;
+
 public class DependencyContainer {
+
+    static class Key {
+
+        @NonNull
+        final Type type;
+
+        @Nullable
+        final String tag;
+
+        Key(@NonNull Type type) {
+            this(type, null);
+        }
+
+        Key(@NonNull Type type, @Nullable String tag) {
+            this.type = type;
+            this.tag = tag;
+        }
+
+        @Override
+        public int hashCode() {
+            int result = type.hashCode();
+            result = 31 * result + (tag != null ? tag.hashCode() : 0);
+            return result;
+        }
+    }
 }
